83d2a12a6ad9c5ff761feecb407869e12c9d9552
joroni <jraymund.niconi@gmail.com>
2018-12-20 11:43:32 +0800

    fixed the server

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -2,7 +2,7 @@ var $$ = Dom7;
 
 var orderItems = localStorage.getItem("txtClients");
 var customers = localStorage.getItem("customers");
-window.base_url = "http://104.238.96.209/~project/joroni/super8/slim";
+window.base_url = "http://128.199.165.197/joroni/super8webapp";
 //var base_url = "http://localhost/slim";
 var currency_icon = '₱';
 $$(".button").addClass("button-big");
@@ -605,7 +605,7 @@ app.pullProductJSONData = function () {
     app.preloader.show();
     // Perform Ajax request
     app.request.get(window.base_url + '/public/api/jsonproducts', function (data) {
-        //app.request.get('http://104.238.96.209/~project/joroni/super8/slim/public/api/jsonproducts', function (data) {
+        //app.request.get('http://128.199.165.197/joroni/super8webapp/public/api/jsonproducts', function (data) {
         // Hide preloader when Ajax request completed
         localStorage.setItem("jsonproducts", data);
 
@@ -624,7 +624,7 @@ app.pullProductData = function () {
     // Perform Ajax request
     //   app.request.get('https://raw.githubusercontent.com/joroni/mysuper8-dev2-copy/master/www/js/data/products.json', function(data){
     app.request.get(window.base_url + '/public/api/products', function (data) {
-        // app.request.get('http://104.238.96.209/~project/joroni/super8/slim/public/api/products', function (data) {
+        // app.request.get('http://128.199.165.197/joroni/super8webapp/public/api/products', function (data) {
         // Hide preloader when Ajax request completed
         localStorage.setItem("products", data);
 
@@ -756,7 +756,7 @@ app.addOrder = function () {
         type: 'POST',
         contentType: 'application/json',
         //url: 'http://localhost/slim/public/api/orders/add',
-        url: window.base_url+'/slim/public/api/orders/add',
+        url: window.base_url+'/public/api/orders/add',
         dataType: "json",
         data: formOrderToJSON(),
         success: function (data, textStatus, jqXHR) {
